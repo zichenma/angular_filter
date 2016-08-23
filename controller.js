713826de3caafac6450c381b9fe20eb689226b0c
zichenma <mazichen1984@gmail.com>
2016-08-23 17:52:26 -0400

    first commit

---
+++ controller.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="angular.js" />
+var myApp = angular
+    .module("myApp", [])
+    .controller("TokenizeControler",
+        function ($scope) {
+            $scope.greeting = "Angular is awesome";
+            $scope.delimiter = "#";
+            $scope.defualt = true;
+    });
---
+++ filter.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="angular.js" />
+/// <reference path="controller.js" />
+
+//Create a filter tokenize
+myApp.filter("tokenize", function () {
+    return function (content, defualt, delim) {
+        if (angular.isString(content)) {
+            var noSpace, output, delimiter;
+            //remove the speace from a string
+            noSpace = content.replace(/ +/g, "");
+            if (defualt) {
+                //defualt filter
+                return output = noSpace.split('').join(',') + " (Default)";
+            } else {
+                //fillter with option
+                delimiter = delim;
+                return output = noSpace.split('').join(delimiter) + "(With option)";
+            }
+        } else {
+            return content;
+        }
+        };
+});
